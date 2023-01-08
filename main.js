368751be30a723e7a50d660989897db938b05ff8
imbharat420 <imbharat420@gmail.com>
2023-01-09 03:45:47 +0530

    group

--- main.js
+++ main.js
@@ -5,45 +5,96 @@ let localTracks = []
 let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
-    let UID = await client.join(config.TOKEN, config.CHANNEL, null);
-    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
 
-    let player = `<div id="video-container" id="user-container-${UID}">
-        <div id="user-${UID}" class="video-player"></div>
-    </div>`
+    client.on('user-published', handleUserJoined)
+    
+    client.on('user-left', handleUserLeft)
+    
+    let UID = await client.join(config.APP_ID, config.CHANNEL, config.TOKEN, null)
 
-    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player);
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks() 
 
-    localTracks[1].play(`user-${UID}`);
-    await client.publish([localTracks[0], localTracks[1]]);
+    let player = `<div class="video-container" id="user-container-${UID}">
+                        <div class="video-player" id="user-${UID}"></div>
+                  </div>`
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+
+    localTracks[1].play(`user-${UID}`)
+    
+    await client.publish([localTracks[0], localTracks[1]])
 }
 
+let joinStream = async () => {
+    await joinAndDisplayLocalStream()
+    document.getElementById('join-btn').style.display = 'none'
+    document.getElementById('stream-controls').style.display = 'flex'
+}
 
-const joinStream = async () => {
-    await joinAndDisplayLocalStream();
-    document.getElementById('join-btn').style.display = 'none';
-    document.getElementById('streams-controls').style.display = 'flex';
-};
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.uid] = user 
+    await client.subscribe(user, mediaType)
 
+    if (mediaType === 'video'){
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if (player != null){
+            player.remove()
+        }
 
+        player = `<div class="video-container" id="user-container-${user.uid}">
+                        <div class="video-player" id="user-${user.uid}"></div> 
+                 </div>`
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
 
-document.getElementById('join-btn').addEventListener('click', joinStream);
+        user.videoTrack.play(`user-${user.uid}`)
+    }
 
+    if (mediaType === 'audio'){
+        user.audioTrack.play()
+    }
+}
 
+let handleUserLeft = async (user) => {
+    delete remoteUsers[user.uid]
+    document.getElementById(`user-container-${user.uid}`).remove()
+}
 
+let leaveAndRemoveLocalStream = async () => {
+    for(let i = 0; localTracks.length > i; i++){
+        localTracks[i].stop()
+        localTracks[i].close()
+    }
 
-/*
-client.init(config.APP_ID, () => {
-    console.log("AgoraRTC client initialized");
-    client.join(config.TOKEN, config.CHANNEL, null, (uid) => {
-        console.log("User " + uid + " join channel successfully");
-        client.publish(localStream, (err) => {
-            console.log("Publish local stream error: " + err);
-        });
-    }, (err) => {
-        console.log("Join channel failed", err);
-    });
-}, (err) => {
-    console.log("AgoraRTC client init failed", err);
-});
-*/
+    await client.leave()
+    document.getElementById('join-btn').style.display = 'block'
+    document.getElementById('stream-controls').style.display = 'none'
+    document.getElementById('video-streams').innerHTML = ''
+}
+
+let toggleMic = async (e) => {
+    if (localTracks[0].muted){
+        await localTracks[0].setMuted(false)
+        e.target.innerText = 'Mic on'
+        e.target.style.backgroundColor = 'cadetblue'
+    }else{
+        await localTracks[0].setMuted(true)
+        e.target.innerText = 'Mic off'
+        e.target.style.backgroundColor = '#EE4B2B'
+    }
+}
+
+let toggleCamera = async (e) => {
+    if(localTracks[1].muted){
+        await localTracks[1].setMuted(false)
+        e.target.innerText = 'Camera on'
+        e.target.style.backgroundColor = 'cadetblue'
+    }else{
+        await localTracks[1].setMuted(true)
+        e.target.innerText = 'Camera off'
+        e.target.style.backgroundColor = '#EE4B2B'
+    }
+}
+
+document.getElementById('join-btn').addEventListener('click', joinStream)
+document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
+document.getElementById('mic-btn').addEventListener('click', toggleMic)
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
